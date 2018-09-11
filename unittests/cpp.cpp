c4f3df0649d9fcfde51f163cfd405c630f868bf6
Thejaswi Rao <rao.thejaswi@gmail.com>
2018-09-11 16:17:57 +0530

    more tests for c++ mode inference

--- unittests/cpp.cpp
+++ unittests/cpp.cpp
@@ -5,10 +5,46 @@
 namespace teditor {
 
 TEST_CASE("CppMode") {
-    Buffer ml;
-    ml.resize({0, 0}, {30, 10});
-    ml.load("samples/sample.cxx", 0);
-    REQUIRE("c++" == ml.modeName());
+    SECTION("cpp file") {
+        Buffer ml;
+        ml.load("test.cpp", 0);
+        REQUIRE("c++" == ml.modeName());
+    }
+    SECTION("c file") {
+        Buffer ml;
+        ml.load("test.c", 0);
+        REQUIRE("c++" == ml.modeName());
+    }
+    SECTION("cxx file") {
+        Buffer ml;
+        ml.load("samples/sample.cxx", 0);
+        REQUIRE("c++" == ml.modeName());
+    }
+    SECTION("cu file") {
+        Buffer ml;
+        ml.load("test.cu", 0);
+        REQUIRE("c++" == ml.modeName());
+    }
+    SECTION("h file") {
+        Buffer ml;
+        ml.load("test.h", 0);
+        REQUIRE("c++" == ml.modeName());
+    }
+    SECTION("hpp file") {
+        Buffer ml;
+        ml.load("test.hpp", 0);
+        REQUIRE("c++" == ml.modeName());
+    }
+    SECTION("cuh file") {
+        Buffer ml;
+        ml.load("test.cuh", 0);
+        REQUIRE("c++" == ml.modeName());
+    }
+    SECTION("Non C/C++ file") {
+        Buffer ml;
+        ml.load("something-else", 0);
+        REQUIRE("c++" != ml.modeName());
+    }
 }
 
 } // end namespace teditor
