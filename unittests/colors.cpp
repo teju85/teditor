12ae025c649acf11dab3d5b1d6a6cbb19e5ab1ba
Thejaswi Rao <rao.thejaswi@gmail.com>
2018-09-11 11:39:56 +0530

    migrated colors.cpp to catch2

---
+++ unittests/colors.cpp
@@ -0,0 +1,35 @@
+#include "colors.h"
+#include "catch.hpp"
+
+namespace teditor {
+
+TEST_CASE("Colors::AttrColor") {
+    AttrColor ac;
+    REQUIRE_FALSE(ac.isBold());
+    REQUIRE_FALSE(ac.isUnderline());
+    ac.setBold();
+    REQUIRE(ac.isBold());
+    ac.setUnderline();
+    REQUIRE(ac.isUnderline());
+    AttrColor ac1, ac2;
+    REQUIRE(ac1 == ac2);
+    REQUIRE(ac != ac1);
+    ac.clearAttr();
+    REQUIRE(ac == ac1);
+    ac.setColor(Color_Red);
+    REQUIRE(Color_Red == ac.color());
+}
+
+TEST_CASE("Colors::AttrHelper") {
+    REQUIRE_THROWS(AttrHelper::fromstr("Invalid"));
+    REQUIRE(Attr_Bold == AttrHelper::fromstr("Bold"));
+    REQUIRE("Bold" == AttrHelper::tostr(Attr_Bold));
+}
+
+TEST_CASE("Colors::ColorHelper") {
+    REQUIRE_THROWS(ColorHelper::fromstr("Invalid"));
+    REQUIRE(Color_Red == ColorHelper::fromstr("Red"));
+    REQUIRE("Red" == ColorHelper::tostr(Color_Red));
+}
+
+} // end namespace teditor
