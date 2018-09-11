c3e11d956130f30e14de6b903f19d191c27b86d2
Thejaswi Rao <rao.thejaswi@gmail.com>
2018-09-11 14:06:01 +0530

    migrated cursor.cpp to catch2

---
+++ unittests/cursor.cpp
@@ -0,0 +1,143 @@
+#include "cursor.h"
+#include "buffer.h"
+#include "catch.hpp"
+
+
+namespace teditor {
+
+TEST_CASE("Cursor::Basics") {
+    Buffer ml;
+    ml.resize({0, 0}, {30, 10});
+    Cursor c;
+    REQUIRE(c.isHidden(0));
+    REQUIRE(c.isHidden(0, Cursor::Hidden));
+    REQUIRE(c.isHidden(Cursor::Hidden, 0));
+    REQUIRE(1 == c.count());
+    c.reset(&ml);
+    REQUIRE_FALSE(c.isHidden(0));
+    REQUIRE(1 == c.count());
+    c.addBack(0, 1);
+    REQUIRE_FALSE(c.isHidden(1));
+    REQUIRE(2 == c.count());
+    c.remove(0);
+    REQUIRE(1 == c.count());
+    REQUIRE(Pos2d<int>(0, 1) == c.at(0));
+
+    c.addBack(1, 2);
+    c.addBack(2, 4);
+    REQUIRE(3 == c.count());
+    c.reset(&ml);
+    REQUIRE(1 == c.count());
+    REQUIRE(Pos2d<int>(0, 0) == c.at(0));
+}
+
+TEST_CASE("Cursor::Movements") {
+    Buffer ml;
+    ml.resize({0, 0}, {30, 10});
+    ml.load("samples/sample.cxx", 0);
+    auto& cu = ml.getCursor();
+
+    REQUIRE(1 == cu.count());
+    REQUIRE_FALSE(cu.isHidden(0));
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.lineEnd(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(ml.at(0).length(), 0) == cu.at(0));
+
+    cu.home(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.right(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(1, 0) == cu.at(0));
+
+    cu.left(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.down(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 1) == cu.at(0));
+
+    cu.up(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.end(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(ml.at(ml.length()-1).length(), ml.length()-1) == cu.at(0));
+
+    cu.reset(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.pageDown(&ml, 1.f);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 9) == cu.at(0));
+
+    cu.pageUp(&ml, 1.f);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.nextPara(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 3) == cu.at(0));
+
+    cu.previousPara(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.nextWord(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(7, 0) == cu.at(0));
+
+    cu.previousWord(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+}
+
+TEST_CASE("Cursor::Operations") {
+    Buffer ml;
+    ml.resize({0, 0}, {30, 10});
+    ml.load("samples/sample.cxx", 0);
+    auto& cu = ml.getCursor();
+
+    cu.remove(0);
+    REQUIRE(1 == cu.count());
+
+    cu.addBack(0, 1);
+    REQUIRE(2 == cu.count());
+    cu.remove(0);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 1) == cu.at(0));
+
+    cu.addFront(0, 0);
+    REQUIRE(2 == cu.count());
+    cu.remove(0);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 1) == cu.at(0));
+
+    cu.addBack(0, 2);
+    REQUIRE(2 == cu.count());
+    cu.reset(&ml);
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.addBack(0, 1);
+    cu.addBack(0, 2);
+    REQUIRE(3 == cu.count());
+    cu.clearAllButFirst();
+    REQUIRE(1 == cu.count());
+    REQUIRE(Pos2d<int>(0, 0) == cu.at(0));
+
+    cu.reset(&ml);
+    REQUIRE(cu.hasCursor(0));
+    REQUIRE_FALSE(cu.hasCursor(1));
+}
+
+
+///@todo: add tests for Regions
+
+} // end namespace teditor
