f088f5f403ab5320c48ffe7de6a38a552bb70373
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-07 16:55:29 +0530

    a few negative tests for common regexs

--- unittests/core/parser/regexs.cpp
+++ unittests/core/parser/regexs.cpp
@@ -22,6 +22,8 @@ TEST_CASE("Regexs::FloatingPt") {
   REQUIRE(nfa.find("-1.2e1") == 5);
   REQUIRE(nfa.find("-1.2E1") == 5);
   REQUIRE(nfa.find("-.2e1") == 4);
+  REQUIRE(nfa.find("hello") == NFA::NoMatch);
+  REQUIRE(nfa.find("1ea") == 0);
 }
 
 TEST_CASE("Regexs::Integer") {
@@ -31,6 +33,7 @@ TEST_CASE("Regexs::Integer") {
   REQUIRE(nfa.find("-1") == 1);
   REQUIRE(nfa.find("+125") == 3);
   REQUIRE(nfa.find("-3701") == 4);
+  REQUIRE(nfa.find("abcd!") == NFA::NoMatch);
 }
 
 TEST_CASE("Regexs::HexInt") {
@@ -38,6 +41,7 @@ TEST_CASE("Regexs::HexInt") {
   REQUIRE(nfa.find("0x1") == 2);
   REQUIRE(nfa.find("0X1") == 2);
   REQUIRE(nfa.find("0X1aA") == 4);
+  REQUIRE(nfa.find("12345") == NFA::NoMatch);
 }
 
 }  // namespace parser
