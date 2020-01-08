7811e39f6eda48c37a5be8845183e1d28a936231
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-08 20:24:49 +0530

    more unit-tests for Lexer class

--- unittests/core/parser/lexer.cpp
+++ unittests/core/parser/lexer.cpp
@@ -77,6 +77,15 @@ TEST_CASE("Lexer") {
     TOKEN_CHECK_STR(lex, sc, Point(7, 0), Point(7, 0), SemiColon);
     TOKEN_CHECK_STR(lex, sc, Point(-1, -1), Point(-1, -1), Token::End);
   }
+  SECTION("disable-print no space") {
+    std::string expr("a=123;");
+    StringScanner sc(expr);
+    TOKEN_CHECK_STR(lex, sc, Point(0, 0), Point(0, 0), Symbol);
+    TOKEN_CHECK_STR(lex, sc, Point(1, 0), Point(1, 0), Assignment);
+    TOKEN_CHECK_STR(lex, sc, Point(2, 0), Point(4, 0), Int);
+    TOKEN_CHECK_STR(lex, sc, Point(5, 0), Point(5, 0), SemiColon);
+    TOKEN_CHECK_STR(lex, sc, Point(-1, -1), Point(-1, -1), Token::End);
+  }
 }
 
 #undef TOKEN_CHECK_STR
