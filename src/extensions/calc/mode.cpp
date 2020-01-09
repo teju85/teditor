2759555f64451041172dafcd4b537061df3af1a8
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-09 22:58:38 +0530

    command to open a *calc buffer

--- src/extensions/calc/mode.cpp
+++ src/extensions/calc/mode.cpp
@@ -1,13 +1,13 @@
-#include "../base/readonly.h"
+#include "../base/text.h"
 #include "core/buffer.h"
 
 namespace teditor {
 namespace calc {
 
 /** calc mode */
-class CalcMode: public readonly::ReadOnlyMode {
+class CalcMode: public text::TextMode {
 public:
-  CalcMode(): readonly::ReadOnlyMode("calc") {
+  CalcMode(): text::TextMode("calc") {
     populateKeyMap<CalcMode::Keys>(getKeyCmdMap());
     populateColorMap<CalcMode::Colors>(getColorMap());
   }
@@ -25,7 +25,108 @@ REGISTER_MODE(CalcMode, "calc");
 
 
 std::vector<KeyCmdPair> CalcMode::Keys::All = {
-  {"q", ""},
+  {" ", "insert-char"},
+  {"!", "insert-char"},
+  {"\"", "insert-char"},
+  {"#", "insert-char"},
+  {"$", "insert-char"},
+  {"%", "insert-char"},
+  {"&", "insert-char"},
+  {"'", "insert-char"},
+  {"(", "insert-char"},
+  {")", "insert-char"},
+  {"*", "insert-char"},
+  {"+", "insert-char"},
+  {"=", "insert-char"},
+  {",", "insert-char"},
+  {"-", "insert-char"},
+  {".", "insert-char"},
+  {"/", "insert-char"},
+  {"0", "insert-char"},
+  {"1", "insert-char"},
+  {"2", "insert-char"},
+  {"3", "insert-char"},
+  {"4", "insert-char"},
+  {"5", "insert-char"},
+  {"6", "insert-char"},
+  {"7", "insert-char"},
+  {"8", "insert-char"},
+  {"9", "insert-char"},
+  {":", "insert-char"},
+  {";", "insert-char"},
+  {"<", "insert-char"},
+  {">", "insert-char"},
+  {"?", "insert-char"},
+  {"@", "insert-char"},
+  {"A", "insert-char"},
+  {"B", "insert-char"},
+  {"C", "insert-char"},
+  {"D", "insert-char"},
+  {"E", "insert-char"},
+  {"F", "insert-char"},
+  {"G", "insert-char"},
+  {"H", "insert-char"},
+  {"I", "insert-char"},
+  {"J", "insert-char"},
+  {"K", "insert-char"},
+  {"L", "insert-char"},
+  {"M", "insert-char"},
+  {"N", "insert-char"},
+  {"O", "insert-char"},
+  {"P", "insert-char"},
+  {"Q", "insert-char"},
+  {"R", "insert-char"},
+  {"S", "insert-char"},
+  {"T", "insert-char"},
+  {"U", "insert-char"},
+  {"V", "insert-char"},
+  {"W", "insert-char"},
+  {"X", "insert-char"},
+  {"Y", "insert-char"},
+  {"Z", "insert-char"},
+  {"[", "insert-char"},
+  {"\\", "insert-char"},
+  {"]", "insert-char"},
+  {"^", "insert-char"},
+  {"_", "insert-char"},
+  {"`", "insert-char"},
+  {"a", "insert-char"},
+  {"b", "insert-char"},
+  {"c", "insert-char"},
+  {"d", "insert-char"},
+  {"e", "insert-char"},
+  {"f", "insert-char"},
+  {"g", "insert-char"},
+  {"h", "insert-char"},
+  {"i", "insert-char"},
+  {"j", "insert-char"},
+  {"k", "insert-char"},
+  {"l", "insert-char"},
+  {"m", "insert-char"},
+  {"n", "insert-char"},
+  {"o", "insert-char"},
+  {"p", "insert-char"},
+  {"q", "insert-char"},
+  {"r", "insert-char"},
+  {"s", "insert-char"},
+  {"t", "insert-char"},
+  {"u", "insert-char"},
+  {"v", "insert-char"},
+  {"w", "insert-char"},
+  {"x", "insert-char"},
+  {"y", "insert-char"},
+  {"z", "insert-char"},
+  {"{", "insert-char"},
+  {"|", "insert-char"},
+  {"}", "insert-char"},
+  {"~", "insert-char"},
+  {"enter", ""},   ///@todo!
+  {"C-K", ""},
+  {"C-U", ""},
+  {"C-R", ""},
+  {"C-Y", ""},
+  {"C-W", ""},
+  {"tab", ""}
 };
 
 std::vector<NameColorPair> CalcMode::Colors::All = {
---
+++ src/extensions/calc/ops.cpp
@@ -0,0 +1,26 @@
+#include "core/editor.h"
+#include "core/command.h"
+#include "core/isearch.h"
+
+namespace teditor {
+namespace calc {
+namespace ops {
+
+Buffer& getCalcBuff(Editor& ed) {
+  bool newOne;
+  auto& buf = ed.getBuff("*calc", true, newOne);
+  if (newOne)
+    buf.setMode(Mode::createMode(Mode::inferMode(buf.bufferName(), false)));
+  return buf;
+}
+
+DEF_CMD(
+  Calc, "calc", DEF_OP() {
+    getCalcBuff(ed);
+    ed.switchToBuff("*calc");
+  },
+  DEF_HELP() { return "Starts the calculator, if not already done."; });
+
+} // end namespace ops
+} // end namespace ledger
+} // end namespace teditor
