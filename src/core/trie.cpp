13abd7241c2f3bc3b1e4126f3e8a25b9df49c033
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-09 16:34:44 +0530

    initial attempt to adding Trie::del

--- src/core/trie.cpp
+++ src/core/trie.cpp
@@ -8,7 +8,7 @@ namespace teditor {
 Node* Node::addNode(const std::string& key) {
   ASSERT(!leaf(), "addNode: adding node to a Leaf! key=%s", key.c_str());
   auto itr = nodes.find(key);
-  if(itr == nodes.end()) {
+  if (itr == nodes.end()) {
     auto* n = new Node;
     nodes[key] = n;
     return n;
@@ -16,9 +16,27 @@ Node* Node::addNode(const std::string& key) {
   return itr->second;
 }
 
+Trie::~Trie() {
+  clear();
+  delete root;
+}
+
 Node* Node::getNode(const std::string& key) {
   auto itr = nodes.find(key);
   return itr == nodes.end() ? nullptr : itr->second;
+}
+
+bool Node::delNode(const Strings& keys, size_t pos) {
+  if (keys.empty()) return false;
+  if (pos == keys.size() - 1) return leaf();
+  auto itr = nodes.find(keys[pos]);
+  if (itr == nodes.end()) return false;
+  auto erased = itr->second->delNode(keys, pos + 1);
+  if (erased) {
+    delete itr->second;
+    nodes.erase(itr);
+  }
+  return erased;
 }
 
 void Node::makeLeaf(const std::string& d) {
@@ -28,22 +46,27 @@ void Node::makeLeaf(const std::string& d) {
 }
 
 void Node::clear() {
-  for(auto itr : nodes) delete itr.second;
+  for (auto itr : nodes) delete itr.second;
   nodes.clear();
 }
 
 
 void Trie::add(const std::string& keys, const std::string& str) {
   // special case of just a space!
-  if(keys == " ") {
-    auto* n = root.addNode(keys);
+  if (keys == " ") {
+    auto* n = root->addNode(keys);
     n->makeLeaf(str);
   }
-  Strings keySeq = split(keys, ' ');
-  if(keySeq.size() <= 0) return;
-  Node* n = &root;
-  for(auto k : keySeq) n = n->addNode(k);
+  auto keySeq = split(keys, ' ');
+  if (keySeq.size() <= 0) return;
+  auto* n = root;
+  for (auto k : keySeq) n = n->addNode(k);
   n->makeLeaf(str);
+}
+
+void Trie::del(const std::string& keys) {
+  auto keySeq = split(keys, ' ');
+  root->delNode(keySeq, 0);
 }
 
 } // end namespace teditor
--- src/core/trie.h
+++ src/core/trie.h
@@ -3,7 +3,7 @@
 #include <unordered_map>
 #include <string>
 #include "keys.h"
-
+#include "utils.h"
 
 namespace teditor {
 
@@ -12,6 +12,7 @@ public:
   Node(): nodes(), isLeaf(false), leafData() {}
   ~Node() { clear(); }
   Node* addNode(const std::string& key);
+  bool delNode(const Strings& keys, size_t pos);
   Node* getNode(const std::string& key);
   void makeLeaf(const std::string& d);
   bool leaf() const { return isLeaf; }
@@ -19,7 +20,7 @@ public:
   void clear();
 
 private:
-  std::unordered_map<std::string,Node*> nodes;
+  std::unordered_map<std::string, Node*> nodes;
   bool isLeaf;
   std::string leafData;
 };
@@ -27,13 +28,15 @@ private:
 
 class Trie {
 public:
-  Trie(): root() {}
+  Trie(): root(new Node) {}
+  ~Trie();
   void add(const std::string& keys, const std::string& str);
-  Node* getNode(const std::string& key) { return root.getNode(key); }
-  void clear() { root.clear(); }
+  void del(const std::string& keys);
+  Node* getNode(const std::string& key) { return root->getNode(key); }
+  void clear() { root->clear(); }
 
 private:
-  Node root;
+  Node* root;
 };
 
 }; // end namespace teditor
