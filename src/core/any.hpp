ce123da8be961d52490684599b6a3912125fe508
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-19 13:29:55 +0530

    unit-tests for Any struct

--- src/core/any.hpp
+++ src/core/any.hpp
@@ -4,9 +4,43 @@
 
 #include <type_traits>
 #include <typeinfo>
+#include <typeindex>
 #include "utils.h"
 
 namespace teditor {
+
+/// struct Impl ///
+template <typename... All> struct Impl;
+
+template <typename Head, typename... Rest>
+struct Impl<Head, Rest...> {
+  static void destroy(const std::type_index& id, void* data) {
+    if (id == typeid(Head)) reinterpret_cast<Head*>(data)->~Head();
+    else Impl<Rest...>::destroy(id, data);
+  }
+
+  static void move(const std::type_index& id, void* newData, void* oldData) {
+    if (id == typeid(Head))
+      new(newData) Head(std::move(*reinterpret_cast<Head*>(oldData)));
+    else
+      Impl<Rest...>::move(id, newData, oldData);
+  }
+
+  static void copy(const std::type_index& id, void* newData, void* oldData) {
+    if (id == typeid(Head))
+      new(newData) Head(*reinterpret_cast<const Head*>(oldData));
+    else
+      Impl<Rest...>::copy(id, newData, oldData);
+  }
+};
+
+template <>
+struct Impl<> {
+  static void destroy(const std::type_index& id, void* data) {}
+  static void move(const std::type_index& id, void* newData, void* oldData) {}
+  static void copy(const std::type_index& id, void* newData, void* oldData) {}
+};
+/// struct Impl ///
 
 /**
  * @brief Data structure to help store any of the pre-defined data types.
@@ -15,43 +49,10 @@ namespace teditor {
 template <typename... Types>
 struct Any {
  private:
-  std::type_info type;
-  std::aligned_union<Types...>::type data;
+  std::type_index type;
+  typename std::aligned_union<1, Types...>::type data;
 
-  /// struct Impl ///
-  template <typename... All> struct Impl;
-
-  template <typename Head, typename... Rest>
-  struct Impl<Head, Rest...> {
-    static void destroy(const std::type_info& id, void* data) {
-      if (id == typeid(Head)) reinterpret_cast<T*>(data)->~Head();
-      else Impl<Rest..>::destroy(id, data);
-    }
-
-    static void move(const std::type_info& id, void* newData, void* oldData) {
-      if (id == typeid(Head))
-        new(newData) Head(std::move(*reinterpret_cast<Head*>(oldData)));
-      else
-        Impl<Rest...>::move(id, newData, oldData);
-    }
-
-    static void copy(const std::type_info& id, void* newData, void* oldData) {
-      if (id == typeid(Head))
-        new(newData) Head(*reinterpret_cast<const Head*>(oldData));
-      else
-        Impl<Rest...>::copy(id, newData, oldData);
-    }
-  };
-
-  template <>
-  struct Impl<> {
-    static void destroy(const std::type_info& id, void* data) {}
-    static void move(const std::type_info& id, void* newData, void* oldData) {}
-    static void copy(const std::type_info& id, void* newData, void* oldData) {}
-  };
-  /// struct Impl ///
-
-  std::type_info invalidType() const { return typeid(void); }
+  std::type_index invalidType() const { return typeid(void); }
 
  public:
   Any(): type(invalidType()) {}
@@ -67,14 +68,15 @@ struct Any {
   ~Any() { Impl<Types...>::destroy(type, &data); }
 
   Any<Types...>& operator=(Any<Types...>&& old) {
+    type = old.type;
     data = old.data;
     old.type = invalidType();
     return *this;
   }
 
   Any<Types...>& operator=(Any<Types...> old) {
-    std::swap(data, old.data);
     std::swap(type, old.type);
+    std::swap(data, old.data);
     return *this;
   }
 
@@ -88,6 +90,13 @@ struct Any {
     ASSERT(type == typeid(T), "Any::get requested-type=%s! storage-type=%s",
            typeid(T).name(), type.name());
     return *reinterpret_cast<T*>(&data);
+  }
+
+  template <typename T>
+  void set(const T& in) {
+    Impl<Types...>::destroy(type, &data);
+    new(&data) T(in);
+    type = typeid(T);
   }
 };  // struct Any
 
---
+++ unittests/core/any.cpp
@@ -0,0 +1,53 @@
+#include "testutils.h"
+#include "core/any.hpp"
+#include "catch.hpp"
+#include <string>
+
+namespace teditor {
+
+TEST_CASE("Any") {
+  Any<bool, int, float, std::string> obj;
+  SECTION("default") {
+    REQUIRE_FALSE(obj.valid());
+    REQUIRE_FALSE(obj.is<bool>());
+    REQUIRE_FALSE(obj.is<int>());
+    REQUIRE_FALSE(obj.is<float>());
+    REQUIRE_FALSE(obj.is<std::string>());
+  }
+  SECTION("bool") {
+    obj.set<bool>(true);
+    REQUIRE(obj.valid());
+    REQUIRE(obj.is<bool>());
+    REQUIRE(obj.get<bool>());
+  }
+  SECTION("int") {
+    obj.set<int>(123);
+    REQUIRE(obj.valid());
+    REQUIRE(obj.is<int>());
+    REQUIRE(obj.get<int>() == 123);
+  }
+  SECTION("int and float") {
+    obj.set<int>(123);
+    REQUIRE(obj.valid());
+    REQUIRE(obj.is<int>());
+    REQUIRE(obj.get<int>() == 123);
+    obj.set<float>(123.4f);
+    REQUIRE(obj.valid());
+    REQUIRE(obj.is<float>());
+    REQUIRE(obj.get<float>() == 123.4f);
+  }
+  SECTION("std::string") {
+    obj.set<std::string>("hello world!");
+    REQUIRE(obj.valid());
+    REQUIRE(obj.is<std::string>());
+    REQUIRE(obj.get<std::string>() == "hello world!");
+  }
+  SECTION("bad usages") {
+    REQUIRE_THROWS(obj.get<bool>());
+    REQUIRE_THROWS(obj.get<int>());
+    REQUIRE_THROWS(obj.get<float>());
+    REQUIRE_THROWS(obj.get<std::string>());
+  }
+}
+
+}  // namespace teditor
