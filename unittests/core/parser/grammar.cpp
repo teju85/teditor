7442c4a742ba76000ab1491d6b0cf7208d789303
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-21 17:50:14 +0530

    covered all cases for grammar construction in unit-tests

--- unittests/core/parser/grammar.cpp
+++ unittests/core/parser/grammar.cpp
@@ -5,20 +5,22 @@
 namespace teditor {
 namespace parser {
 
-TEST_CASE("Grammar") {
+TEST_CASE("Grammar Construction") {
   Grammar g;
-  SECTION("case 1") {
-    g.addTerminal("Int", Regexs::Integer);
-    g.addTerminal("+", "[+]");
-    g.addNonTerminal("Add", {"Int", "+", "Int"});
-    REQUIRE(0 == g.getId("Int"));
-    REQUIRE(1 == g.getId("+"));
-    REQUIRE(2 == g.getId("Add"));
-    REQUIRE_THROWS(g.getId("NotThere"));
-    REQUIRE("Int" == g.getName(0));
-    REQUIRE("+" == g.getName(1));
-    REQUIRE("Add" == g.getName(2));
-  }
+  g.addTerminal("Int", Regexs::Integer);
+  g.addTerminal("+", "[+]");
+  // duplicate terminals aren't allowed
+  REQUIRE_THROWS(g.addTerminal("Int", Regexs::Integer));
+  g.addNonTerminal("Add", {"Int", "+", "Int"});
+  // can't add terminals after adding non-terminals!
+  REQUIRE_THROWS(g.addTerminal("+", "[+]"));
+  REQUIRE(0 == g.getId("Int"));
+  REQUIRE(1 == g.getId("+"));
+  REQUIRE(2 == g.getId("Add"));
+  REQUIRE_THROWS(g.getId("NotThere"));
+  REQUIRE("Int" == g.getName(0));
+  REQUIRE("+" == g.getName(1));
+  REQUIRE("Add" == g.getName(2));
 }
 
 } // end namespace parser
