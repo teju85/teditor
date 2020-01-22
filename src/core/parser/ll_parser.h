988bfb0b16e05232ffa9507fe2ebc0cfc6a02d47
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-22 23:23:21 +0530

    clean up of FIRST set calculation logic for LL parser

--- src/core/parser/ll_parser.h
+++ src/core/parser/ll_parser.h
@@ -13,7 +13,8 @@ template <int k> struct LL;
 template <>
 struct LL<1> {
   LL(const Grammar& g): table(), lexer(g.getLexer()),
-                        epsId(g.getId(Grammar::Eps)) {
+                        epsId(g.getId(Grammar::Eps)),
+                        eofId(g.getId(Grammar::Eof)) {
     constructTable(g);
   }
 
@@ -24,19 +25,23 @@ struct LL<1> {
   Table table;
   std::shared_ptr<Lexer> lexer;
   uint32_t epsId;
+  uint32_t eofId;
 
   typedef std::unordered_set<uint32_t> First;
   typedef std::unordered_map<uint32_t, First> FirstMap;
 
   void constructTable(const Grammar& g) {
-    FirstMap firsts;
+    FirstMap firsts, follows;
+    auto start = g.getId(g.getStart());
+    follows[start] = {eofId};
     const auto nProds = g.numProductions();
-    // construct FIRST sets
+    // construct FIRST and FOLLOW sets
     for (uint32_t i = 0; i < nProds; ++i) {
       First prodFirst;
+      const auto& rhs = g.getRhs(i);
       // take a union of all symbols in the RHS, while checking for 'eps'
-      for (const auto& rhs : g.getRhs(i)) {
-        auto rid = g.getId(rhs);
+      for (const auto& r : rhs) {
+        auto rid = g.getId(r);
         const auto& f = getFirstFor(g, rid, firsts);
         for (auto fi : f) prodFirst.insert(fi);
         // if FIRST(rhs) doesn't contain 'eps', then halt
@@ -57,10 +62,18 @@ struct LL<1> {
                tok, g.getName(tok).c_str(), itr2->second);
         itr->second[tok] = i;
       }
-    }
-    // construct FOLLOW sets
-    FirstMap follows;
-    for (uint32_t i = 0; i < nProds; ++i) {
+      // populate the parse table based on FOLLOW sets
+      if (prodFirst.find(epsId) != f.end()) {
+        const auto& f = getFollowFor(g, lid, firsts, follows);
+        for (const auto& tok : f) {
+          auto itr2 = itr->second.find(tok);
+          ASSERT(itr2 == itr->second.end(),
+                 "LL<1>: symbol=%u(%s) found a conflict in parseTable for"
+                 " token=%u(%s) at production=%u!", lid, g.getName(lid).c_str(),
+                 tok, g.getName(tok).c_str(), itr2->second);
+          itr->second[tok] = i;
+        }
+      }
     }
   }
 
@@ -81,6 +94,57 @@ struct LL<1> {
           continue;
         }
         const auto& f = getFirstFor(g, rid, firsts);
+        for (auto fi : f) myFirst.insert(fi);
+        // if FIRST(rid) doesn't contain 'eps', then halt
+        if (f.find(epsId) == f.end()) break;
+      }
+    }
+    firsts[id] = myFirst;
+    return firsts.find(id)->second;
+  }
+
+  const First& getFollowFor(const Grammar& g, uint32_t id,
+                            const FirstMap& firsts, FirstMap& follows) {
+    // if FOLLOW(id) already exists, just return it
+    const auto itr = follows.find(id);
+    if (itr != follows.end()) return itr->second;
+    // else, compute it recursively via all of the production rules, where this
+    // symbol occurs in lhs and get their union
+    const auto& pids = g.getProdIds(id);
+    for (auto p : pids) {
+      const auto& rhs = g.getRhs(p);
+      if (rhs.size() >= 3) {
+        // FIRST(last_symbol) -> FOLLOW(last_but_one_symbol)
+        auto last = g.getId(rhs.back());
+        auto lastButOne = g.getId(rhs[rhs.size() - 2]);
+        if (follows.find(lastButOne) == follows.end())
+          follows[lastButOne] = First();
+        const auto& f = getFirstFor(g, last, firsts);
+        for (auto fi : f) {
+          if (fi != epsId) follows[lastButOne].insert(fi);
+        }
+        // FOLLOW(lhs) -> FOLLOW(rhs_last_but_one_symbol)
+        if (f.find(epsId) != f.end()) {
+          const auto& f= getFollowFor(g, lid, firsts, follows);
+          for (auto fi : f) follows[lastButOne].insert(fi);
+        }
+      }
+      // FOLLOW(lhs) -> FOLLOW(rhs_last_symbol)
+      if(rhs.size() >= 2) {
+        auto lastId = g.getId(rhs.back());
+        if (follows.find(lastId) == follows.end()) follows[lastId] = First();
+        const auto& f = getFollowFor(g, lid, firsts, follows);
+        for (auto fi : f) follows[lastId].insert(fi);
+      }
+
+      // take a union of FIRST of all symbols in the RHS
+      for (const auto& rhs : g.getRhs(p)) {
+        auto rid = g.getId(rhs);
+        if (g.isTerminal(rid)) {
+          myFirst.insert(rid);
+          continue;
+        }
+        const auto& f = getFollowFor(g, rid, firsts, follows);
         for (auto fi : f) myFirst.insert(fi);
         // if FIRST(rid) doesn't contain 'eps', then halt
         if (f.find(epsId) == f.end()) break;
