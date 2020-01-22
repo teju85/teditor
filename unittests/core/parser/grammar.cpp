5315a850f1137438351e13ae316b80fbd7e63ea8
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-22 11:51:44 +0530

    added more rules for better unit-testing of Grammar

--- unittests/core/parser/grammar.cpp
+++ unittests/core/parser/grammar.cpp
@@ -8,26 +8,50 @@ namespace parser {
 TEST_CASE("Grammar Construction") {
   Grammar g({
       {"Int", Regexs::Integer},
-      {"+", "[+]"}
+      {"+", "[+]"},
+      {"-", "-"},
     }, {
-      {"Add", {"Int", "+", "Int"}}
+      {"Add", {"Int", "+", "Int"}},
+      {"Sub", {"Int", "-", "Int"}},
+      {"Expr", {"Add"}},
+      {"Expr", {"Sub"}},
+      {"Statement", {"Expr"}},
     },
-    "Add");
+    "Statement");
 
   REQUIRE(0 == g.getId("Int"));
   REQUIRE(1 == g.getId("+"));
-  REQUIRE(2 == g.getId("Add"));
+  REQUIRE(2 == g.getId("-"));
+  REQUIRE(3 == g.getId("Add"));
+  REQUIRE(4 == g.getId("Sub"));
+  REQUIRE(5 == g.getId("Expr"));
+  REQUIRE(6 == g.getId("Statement"));
   REQUIRE_THROWS(g.getId("NotThere"));
   REQUIRE("Int" == g.getName(0));
   REQUIRE("+" == g.getName(1));
-  REQUIRE("Add" == g.getName(2));
+  REQUIRE("-" == g.getName(2));
+  REQUIRE("Add" == g.getName(3));
+  REQUIRE("Sub" == g.getName(4));
+  REQUIRE("Expr" == g.getName(5));
+  REQUIRE("Statement" == g.getName(6));
+
   REQUIRE(g.isTerminal("Int"));
   REQUIRE(g.isTerminal("+"));
+  REQUIRE(g.isTerminal("-"));
   REQUIRE_FALSE(g.isTerminal("Add"));
+  REQUIRE_FALSE(g.isTerminal("Sub"));
+  REQUIRE_FALSE(g.isTerminal("Expr"));
+  REQUIRE_FALSE(g.isTerminal("Statement"));
   REQUIRE_FALSE(g.isTerminal("NotThere"));
   REQUIRE(g.isTerminal(0));
   REQUIRE(g.isTerminal(1));
-  REQUIRE_FALSE(g.isTerminal(2));
+  REQUIRE(g.isTerminal(2));
+  REQUIRE_FALSE(g.isTerminal(3));
+  REQUIRE_FALSE(g.isTerminal(4));
+  REQUIRE_FALSE(g.isTerminal(5));
+  REQUIRE_FALSE(g.isTerminal(6));
+
+  REQUIRE("Statement" == g.getStart());
 
   REQUIRE_THROWS(g.getProdIds("Int"));
   REQUIRE_THROWS(g.getProdIds("NotThere"));
@@ -44,8 +68,8 @@ TEST_CASE("Grammar Construction") {
   REQUIRE("+" == rhs[1]);
   REQUIRE("Int" == rhs[2]);
 
-  REQUIRE(1 == g.numNonTerminals());
-  REQUIRE(1 == g.numProductions());
+  REQUIRE(4 == g.numNonTerminals());
+  REQUIRE(5 == g.numProductions());
 }
 
 } // end namespace parser
