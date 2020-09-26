20c387668edf217ca48f699a293e63de77a9733b
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-09-27 01:07:22 +0530

    removed old pcre-based logic from ledger parser

--- src/extensions/ledger/parser.cpp
+++ src/extensions/ledger/parser.cpp
@@ -34,11 +34,7 @@ parser::Grammar& getGrammar() {
 }
 
 Parser::Parser(const std::string& f):
-  file(f), trans(), accts(),
-  accState(), accRx("^account\\s+(\\S+)"), accDescRx("^  description\\s+(.*)"),
-  accAliasRx("^  alias\\s+(\\S+)"),
-  traRx("^(\\d+/\\d+/\\d+)\\s+(.*)"),
-  traOpRx("^  (\\S+)\\s+(-?\\d+[.]?(\\d+)?)"), traOpDefRx("^  (\\S+)$") {
+  file(f), trans(), accts() {
   parse(file);
 }
 
@@ -122,67 +118,9 @@ void Parser::parse(const std::string& f) {
   for(auto& t : trans) t.updateAccounts(accts);
 }
 
-// void Parser::parse(const std::string& f) {
-//   std::fstream fp;
-//   fp.open(f.c_str(), std::fstream::in);
-//   ASSERT(fp.is_open(), "Failed to read ledger file '%s'", f.c_str());
-//   std::string str;
-//   size_t line;
-//   while(std::getline(fp, str)) {
-//     // empty line or comments
-//     if(str.empty() || str[0] == '#') continue;
-//     // account definition
-//     auto mAcc = accRx.find(str);
-//     if(!mAcc.empty()) {
-//       accState.name = mAcc.get(1);
-//       while(true) {
-//         ++line;
-//         std::string s;
-//         std::getline(fp, s);
-//         auto m1 = accDescRx.find(s);
-//         auto m2 = accAliasRx.find(s);
-//         if(!m1.empty()) {
-//           accState.desc = m1.get(1);
-//         } else if(!m2.empty()) {
-//           accState.als.insert(m2.get(1));
-//         } else {
-//           --line;
-//           accts.push_back(Account(accState.name, accState.desc, accState.als));
-//           accState.clear();
-//           break;
-//         }
-//       }
-//       continue;
-//     } // end account definition
-//     // transactions
-//     auto mTra = traRx.find(str);
-//     if(!mTra.empty()) {
-//       Transaction tr(mTra.get(1), mTra.get(2));
-//       while(true) {
-//         ++line;
-//         std::string s;
-//         std::getline(fp, s);
-//         auto m1 = traOpRx.find(s);
-//         auto m2 = traOpDefRx.find(s);
-//         if(!m1.empty()) {
-//           tr.add(m1.get(1), str2double(m1.get(2)));
-//         } else if(!m2.empty()) {
-//           tr.add(m2.get(1));
-//         } else {
-//           --line;
-//           trans.push_back(tr);
-//           break;
-//         }
-//       }
-//     } // end transactions
-//   } // end while
-//   for(auto& t : trans) t.updateAccounts(accts);
-// }
-
 void Parser::reload() {
   trans.clear();
   accts.clear();
-  accState.clear();
   parse(file);
 }
 
--- src/extensions/ledger/parser.h
+++ src/extensions/ledger/parser.h
@@ -1,15 +1,11 @@
 #pragma once
 
 #include "objects.h"
-#include "core/pcre.h"
 
 namespace teditor {
 namespace ledger {
 
-/**
- * Poor man's ledger parser!
- * Currently it is just a hacky collection of Pcre2 regex's
- */
+/** Poor man's ledger parser! */
 class Parser {
 public:
   Parser(const std::string& f);
@@ -39,23 +35,9 @@ public:
   void minmaxDates(Date& min, Date& max) const;
 
 private:
-  struct AccState {
-    std::string name, desc;
-    Aliases als;
-
-    void clear() {
-      name.clear();
-      desc.clear();
-      als.clear();
-    }
-  };
-
   std::string file;
   Transactions trans;
   Accounts accts;
-  AccState accState;
-  Pcre accRx, accDescRx, accAliasRx;
-  Pcre traRx, traOpRx, traOpDefRx;
 
   void parse(const std::string& f);
 };
