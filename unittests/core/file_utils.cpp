53060ddba1ef2a1769ee4b5b47d48e4285113cb5
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-10-17 17:59:51 +0530

    removed the listDir2str method unittest as its output varies as per different machines!

--- unittests/core/file_utils.cpp
+++ unittests/core/file_utils.cpp
@@ -4,17 +4,6 @@
 
 namespace teditor {
 
-TEST_CASE("FileUtils::listDir2str") {
-  auto fs = listDir("samples");
-  std::string ref;
-  for(const auto& f : fs) {
-    auto fname = isCurrentOrParentDir(f.name)? f.name : basename(f.name);
-    auto buff = format("  %10.10s  %8lu  %s\n", f.perms, f.size, fname.c_str());
-    ref += buff;
-  }
-  REQUIRE(ref == listDir2str("samples"));
-}
-
 TEST_CASE("FilesHist") {
     const std::string file("test_hist.txt");
     remove(file.c_str());
