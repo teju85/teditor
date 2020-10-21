e2be8a972ae8bd79273deb437db768d5a00053a3
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-10-21 09:02:25 +0530

    added debug prints to locate the source of error

--- .github/actions/gh-pages/run.sh
+++ .github/actions/gh-pages/run.sh
@@ -16,22 +16,25 @@ build_dir="dist"
 
 cd "$GITHUB_WORKSPACE"
 
+echo "Setting up user info..."
 git config user.name "$GITHUB_ACTOR"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
+echo "Building docs..."
 make doc
-git branch -D "$target_branch"
-git fetch "$remote_name" "$target_name:$target_name"
+echo "Switching to gh-pages..."
 git checkout "$target_branch"
+echo "Removing old documentation..."
 rm -rf *.html *.js *.css *.png search/
+echo "Copying the new documentation..."
 cp -r bin/html/html/* .
-
+echo "Committing..."
 git add .
 git commit -m "updated GitHub Pages"
 if [ $? -ne 0 ]; then
     echo "nothing to commit"
     exit 0
 fi
-
+echo "Pushing..."
 git remote set-url "$remote_name" "$repo_uri" # includes access token
 git push --force-with-lease "$remote_name" "$target_branch"
